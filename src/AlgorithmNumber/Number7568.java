8252a248bd3c47edaf29347365b434e75e7d4acc
SooYoungJang <tturrr10@gmail.com>
2021-07-29 14:06:14 +0900

    Number 7568번

--- src/AlgorithmNumber/Number7568.java
+++ src/AlgorithmNumber/Number7568.java
@@ -10,7 +10,44 @@ import java.util.List;
 public class Number7568 {
 
     public static void main(String[] args) throws IOException {
+        //N 만큼의 사람이 입력된다.
+        //N 은 몸무게 x와 키 y로 이루어져 있다.
+        //N의 순위를 구한다.
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> grade = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+        String[] people = new String[N];
+        for (int i = 0; i < N; i++) {
+            people[i] = br.readLine();
+        }
+
+        for (int i =0; i < people.length; i++) {
+            String[] duff = people[i].split(" ");
+            grade.add(getGrade(people,duff[0],duff[1]));
+        }
+
+        for (int i =0; i < grade.size(); i++) {
+            sb.append(grade.get(i).toString()).append(" ");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+    private static int getGrade(String[] people, String personX, String personY) {
+        int count =1;
+        int personXInt = Integer.parseInt(personX);
+        int personYInt = Integer.parseInt(personY);
+        for (int i =0; i< people.length; i++) {
+            String[] duff = people[i].split(" ");
+            int weight = Integer.parseInt(duff[0]);
+            int height = Integer.parseInt(duff[1]);
+            if(personXInt < weight && personYInt < height) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        //방법 1.
-        // Collection.sort 사용
-        // N개의 숫자를 받는다
-        // N개 만큼의 입력을 List에 받는다
-        // Collections. sort 를 사용하여 출력하였더니 메모리 초과가 생겻다.
-        // 이문제에 메모리제한은 3mb이다.
-
-        //방법1
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        int count = Integer.parseInt(br.readLine());
-//        List<String> nums = new LinkedList<>();
-//        for (int i =0; i<count; i++) {
-//            nums.add(br.readLine());
-//        }
-
-//        Collections.sort(nums);
-//
-//        for(String i : nums) {
-//            bw.write(i+"\n");
-//            bw.flush();
-//        }
-
-        //방법2
-        //Countingsort 알고리즘 사용.
+        //N 만큼의 사람이 입력된다.
+        //N 은 몸무게 x와 키 y로 이루어져 있다.
+        //N의 순위를 구한다.
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int[] cnt = new int[10001]; //       수의 범위 (0 ~ 10000) 사실상 0은 제외
+        ArrayList<Integer> grade = new ArrayList<>();
         int N = Integer.parseInt(br.readLine());
+        String[] people = new String[N];
         for (int i = 0; i < N; i++) {
-            // 해당 인덱스의 값을 1 증가
-            cnt[Integer.parseInt(br.readLine())] ++;
+            people[i] = br.readLine();
         }
-        // 0은 입력범위에서 없으므로 1부터 시작
-        for(int i = 1; i < 10001; i++){
-            // i 값이 개수가 0 이 될 때 까지 출력 (빈도수를 의미)
-            while(cnt[i] > 0){
-                sb.append(i).append('\n');
-                cnt[i]--;
+
+        for (int i =0; i < people.length; i++) {
+            String[] duff = people[i].split(" ");
+            grade.add(getGrade(people,duff[0],duff[1]));
+        }
+
+        for (int i =0; i < grade.size(); i++) {
+            sb.append(grade.get(i).toString()).append(" ");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+    private static int getGrade(String[] people, String personX, String personY) {
+        int count =1;
+        int personXInt = Integer.parseInt(personX);
+        int personYInt = Integer.parseInt(personY);
+        for (int i =0; i< people.length; i++) {
+            String[] duff = people[i].split(" ");
+            int weight = Integer.parseInt(duff[0]);
+            int height = Integer.parseInt(duff[1]);
+            if(personXInt < weight && personYInt < height) {
+                count++;
             }
         }
-        System.out.println(sb);
-
+        return count;
     }
 }
