358e017d0b1805ba27204e7efa8d4834d8fc1b1e
SooYoungJang <tturrr10@gmail.com>
2021-08-06 15:36:41 +0900

    Number10828 add.

--- src/AlgorithmNumber/Number10828.java
+++ src/AlgorithmNumber/Number10828.java
@@ -1,9 +1,11 @@
+package AlgorithmNumber;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main {
+public class Number10828 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Integer> stack = new Stack<>();
@@ -50,5 +52,6 @@ public class Main {
         System.out.println(sb);
     }
 
-}
 
+
+}
