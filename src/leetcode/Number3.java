4606a70b06f84bc168436fe559d48d09fd8ca1ad
eddy <tturrr10@gmail.com>
2021-11-04 22:30:24 +0900

    leetcode Number3 add

---
+++ src/leetcode/Number3.java
@@ -0,0 +1,25 @@
+package src.leetcode;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Number3 {
+
+    public static void main(String[] args) throws IOException {
+        lengthOfLongestSubstring("ll");
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max=0;
+        for (int i=0, j=0; i<s.length(); ++i){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-j+1);
+        }
+        return max;
+    }
+}
