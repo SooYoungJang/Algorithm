1caf5e14e6b031494a01e59e1c00da08bf5fca8b
SooYoungJang <tturrr10@gmail.com>
2021-11-10 22:10:54 +0900

    leetcode Number48 add

---
+++ src/leetcode/Number48.java
@@ -0,0 +1,28 @@
+package src.leetcode;
+
+import java.io.IOException;
+
+public class Number48 {
+    public static void main(String[] args) throws IOException {
+        rotate(new int[][]{{1,2,3,4,},{1,2,3,3}});
+    }
+
+    public static void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i =0 ; i<matrix.length; i++){
+            for(int j = 0; j<matrix.length/2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+}
